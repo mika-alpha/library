d9047c900179b5a0e354469b68fd179157664d6c
mika-alpha <yuukeikioku@gmail.com>
2022-06-15 15:56:39 -0500

    Add functionality to the modal

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
+let modal = document.querySelector('.modal');
+let closeButton = document.querySelector('.close');
+let addButton = document.querySelector('.new-book');
+let form = document.forms.bookForm;
+let addBookButton = document.getElementById('add');
+
 let library = [];
+
+
+closeButton.addEventListener('click', closeModal);
+addButton.addEventListener('click', openModal);
+addBookButton.addEventListener('click', addBookToLibrary);
+
+function closeModal(){
+    modal.style.display = "none";
+}
+
+function openModal(){
+    modal.style.display = "flex";
+}
+
+function addBookToLibrary(){
+    let formData = new FormData(form);
+    library.push(new Book(formData.get('title'), formData.get('author'), 
+    Number(formData.get('pages')), Boolean(Number(formData.get('read')))));
+    form.reset();
+    closeModal();
+}
+
+
 function Book(title, author, pages, isRead){
     this.title = title;
     this.author = author;
@@ -8,8 +37,4 @@ function Book(title, author, pages, isRead){
 
 Book.prototype.info = function(){
     return `${this.title} by ${this.author}, ${this.pages}, ${this.isRead? 'read' : 'not read yet'}`;
-}
-
-function addBook(book){
-    library.push(book)
 }
